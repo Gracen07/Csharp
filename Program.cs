94e828528fe6f3abe16cef5bec5661060bc7900d
Gracen07 <125753223+Gracen07@users.noreply.github.com>
2024-06-06 18:16:54 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Hello
+{
+    public class Program
+    {
+        public Program()
+        {
+            Console.WriteLine("Enter a day");
+            int day=int.Parse(Console.ReadLine());
+            switch (day)
+            {
+                case 0:
+                    Console.WriteLine("SUNDAY");
+                    break;
+                case 1:
+                    Console.WriteLine("MONDAY");
+                    break;
+                case 2:
+                    Console.WriteLine("TUESDAY");
+                    break;
+                default:
+                    Console.WriteLine("enter a valid number between 0 and 6");
+                    break;
+            }
+            }
+    }
+}
