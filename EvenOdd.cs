049ef4fdd5aff6fb6756acebc56603357cb5c0b0
Gracen07 <125753223+Gracen07@users.noreply.github.com>
2024-06-07 21:26:55 +0530

    Add files via upload

---
+++ EvenOdd.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Hello;
+public class EvenOdd
+{
+	public EvenOdd()
+	{
+        Console.WriteLine("Enter a number:");
+        int b = int.Parse(Console.ReadLine());
+		if (b % 2 == 0)
+		{
+			Console.WriteLine("Even");
+		}
+		else
+		{
+			Console.WriteLine("ODD");
+		}
+
+	}
+	
+}
