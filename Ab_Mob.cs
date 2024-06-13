3bd44af24d7ddaad2b6c01c8f26588c51d095cf2
Gracen07 <125753223+Gracen07@users.noreply.github.com>
2024-06-13 18:55:51 +0530

    Add files via upload

---
+++ Ab_Mob.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Mob
+{
+    public abstract class Ab_Mob
+    {
+        protected string brand;
+        protected string model;
+        protected int batteryLevel;
+
+        public Ab_Mob(string brand,string model,int batterLevel)
+        {
+            this.brand = brand;
+            this.model = model; 
+            this.batteryLevel = batterLevel;    
+        }
+
+        public abstract void StartDevice();
+        public abstract void UseDevice();
+        public virtual void ShowDetails()
+        {
+            Console.WriteLine($"Brand:{ brand},model:{ model},batterLevel:{batteryLevel}");
+        }
+    }
+}
