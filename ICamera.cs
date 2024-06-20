ff8c52af98a5c17e07a5eea32e9d60bce9637e0d
Gracen07 <125753223+Gracen07@users.noreply.github.com>
2024-06-20 22:17:03 +0530

    Add files via upload

---
+++ ICamera.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mob_Inherited
+{
+    public interface ICamera
+    { 
+        public void TakePhoto();
+    }
+}
