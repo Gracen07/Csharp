58c538a2c65fc4d7a30f5bed72f979744290d4b8
Gracen07 <125753223+Gracen07@users.noreply.github.com>
2024-06-20 22:16:41 +0530

    Add files via upload

---
+++ Android.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mob_Inherited
+{
+    public class Android:Mob,ICamera,IGps
+    {
+        public int version;
+        public Android(string brand,string model,int version): base(brand,model)
+        {
+            this.version = version;
+        }
+        public void PrintVersion()
+        {
+            Console.WriteLine("Version "+version);
+        }
+
+        public void TakePhoto()
+        {
+            Console.WriteLine("Taking photo");
+        }
+
+        public void GetGps()
+        {
+            Console.WriteLine("Current Location");
+        }
+    }
+}
